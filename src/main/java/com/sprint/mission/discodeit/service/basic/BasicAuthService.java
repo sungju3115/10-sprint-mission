b42eaa88faf8c0fe036f7a4706b69d7ebe9f0c23
jsj <sungju3115@naver.com>
2026-03-26 08:34:55 +0900

    refactor(service): BasicAuthService 인증 로직 정리

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -1,4 +1,4 @@
-package com.sprint.mission.discodeit.service.auth;
+package com.sprint.mission.discodeit.service.basic;
 
 import com.sprint.mission.discodeit.dto.auth.request.AuthServiceRequest;
 import com.sprint.mission.discodeit.dto.user.response.UserDTO;
@@ -6,27 +6,30 @@ import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.mapper.AuthMapper;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.repository.UserStatusRepository;
+import com.sprint.mission.discodeit.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.NoSuchElementException;
+
 @RequiredArgsConstructor
 @Service
-public class AuthService {
+public class BasicAuthService implements AuthService {
     private final UserRepository userRepository;
-    private final UserStatusRepository userStatusRepository;
     private final AuthMapper authMapper;
+
     @Transactional(readOnly = true)
     public UserDTO login(AuthServiceRequest request){
         // name 같은 지 확인
         User user = userRepository.findByUsernameWithProfile(request.username())
-                .orElseThrow(() -> new IllegalArgumentException("User not found: " + request.username()));
+                .orElseThrow(() -> new NoSuchElementException("User not found: " + request.username()));
 
         // password 같은지 확인
         if(!(user.getPassword().equals(request.password()))){
             throw new IllegalArgumentException("Wrong password");
         }
-        // 유저 정보 반환, DTO로 ??
+        // 유저 dto로 변환
         return authMapper.toResponse(user);
     }
 }
