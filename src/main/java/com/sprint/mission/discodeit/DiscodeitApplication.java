6824db761524c42d22538c401af15cba12382f40
jsj <sungju3115@naver.com>
2026-04-09 09:30:54 +0900

    refactor: 안쓰는 코드 삭제

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -1,20 +1,13 @@
 package com.sprint.mission.discodeit;
 
-import com.sprint.mission.discodeit.service.ChannelService;
-import com.sprint.mission.discodeit.service.MessageService;
-import com.sprint.mission.discodeit.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
 @SpringBootApplication
 public class DiscodeitApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
-
-		ChannelService channelService = context.getBean(ChannelService.class);
-		UserService userService = context.getBean(UserService.class);
-		MessageService messageService = context.getBean(MessageService.class);
-
 	}
 }
