eaaf2cf7c57d3009e947c42f77765ae39948e074
jsj <sungju3115@naver.com>
2026-05-14 14:27:51 +0900

    feat: API 추가 및 헤더 추가

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.dto.auth.AuthServiceRequest;
 import com.sprint.mission.discodeit.dto.user.response.UserDTO;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.service.basic.BasicAuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -12,8 +13,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import jakarta.validation.Valid;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 
@@ -59,10 +63,18 @@ public class AuthController {
 //        return ResponseEntity.ok(authService.login(request));
 //    }
 
-    @GetMapping("csrf-token")
+    @GetMapping("/csrf-token")
     public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
         String tokenValue = csrfToken.getToken();
         log.debug("CSRF Token: {}", tokenValue);
-        return ResponseEntity.ok().header("X-CSRF-TOKEN", tokenValue).build();
+        return ResponseEntity
+                .status(HttpStatus.NON_AUTHORITATIVE_INFORMATION)
+                .header("X-CSRF-TOKEN", tokenValue)
+                .build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDTO> getUserDto(@AuthenticationPrincipal UserDetails userDetails){
+        return ResponseEntity.ok(((DiscodeitUserDetails) userDetails).getUserDTO());
     }
 }
