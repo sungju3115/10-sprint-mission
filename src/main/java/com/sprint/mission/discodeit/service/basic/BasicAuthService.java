a77dab6d2f42c4faa8071f05af1a78b0dc314308
jsj <sungju3115@naver.com>
2026-05-18 10:17:28 +0900

    fix: filteChain 순서 변경, NPE 버그 수정, 역할 변경 시 세션 무효화 추가

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -6,9 +6,12 @@ import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.exception.user.UserNotFoundException;
 import com.sprint.mission.discodeit.mapper.UserMapper;
 import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class BasicAuthService implements AuthService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final SessionRegistry sessionRegistry;
 
     @Override
     @Transactional
@@ -28,6 +32,13 @@ public class BasicAuthService implements AuthService {
 
         // role update , 동시에 같은 유저의 role을 바꾸는 상황은 X
         user.updateRole(request.newRole());
+
+        // 역할 변경된 유저의 활성 모든 세션 만료 및 재로그인
+        sessionRegistry.getAllPrincipals().stream()
+                .map(p -> (DiscodeitUserDetails) p)
+                .filter(p -> p.getUserDTO().id().equals(user.getId()))
+                .flatMap(p -> sessionRegistry.getAllSessions(p, false).stream())
+                .forEach(SessionInformation::expireNow);
 
         // 명시적 save X, find로 영속성 컨텍스트에 user가 올라있기 때문
         return userMapper.toDTO(user);
