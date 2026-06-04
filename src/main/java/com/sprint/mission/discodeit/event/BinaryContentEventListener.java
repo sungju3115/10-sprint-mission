e29b59e0fd90681a681e83cffdade087b71f9097
jsj <sungju3115@naver.com>
2026-06-04 14:30:21 +0900

    feat: AsyncConfig 구현 - @EnableAsync, ThreadPoolTaskExecutor, MdcTaskDecorator 추가

--- src/main/java/com/sprint/mission/discodeit/event/BinaryContentEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/BinaryContentEventListener.java
@@ -4,7 +4,10 @@ import com.sprint.mission.discodeit.entity.BinaryContentStatus;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -14,11 +17,9 @@ public class BinaryContentEventListener {
     private final BinaryContentStorage binaryContentStorage;
     private final BinaryContentService binaryContentService;
 
-    // 메타 데이터 커밋 이후 바이너리 데이터 저장 : AFTER_COMMIT
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    // @Transactional(propagation = Propagation.REQUIRES_NEW)
-    // Todo: DB 트랜잭션 점유를 이벤트 발행 메서드로 가져가는 것 보다는 Status Update만 점유하도록 하는 게 맞나 ?
-    // 트랜잭션 A -> 커밋 -> 이벤트 발행 -> storage.put -> 새 트랜잭션 (updateStatus)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void handleBinaryContentCreatedEvent(BinaryContentCreatedEvent event) {
         try {
             binaryContentStorage.put(event.getBinaryContentId(), event.getBytes());
