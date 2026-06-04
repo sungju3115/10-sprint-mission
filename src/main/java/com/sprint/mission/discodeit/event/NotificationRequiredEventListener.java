df811718a16066ee436e9f79dac999abde701145
jsj <sungju3115@naver.com>
2026-06-04 14:30:29 +0900

    feat: NotificationRequiredEventListener에 @Async 적용

--- src/main/java/com/sprint/mission/discodeit/event/NotificationRequiredEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/NotificationRequiredEventListener.java
@@ -3,6 +3,7 @@ package com.sprint.mission.discodeit.event;
 import com.sprint.mission.discodeit.dto.notification.NotificationCreateRequest;
 import com.sprint.mission.discodeit.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,24 +14,22 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class NotificationRequiredEventListener {
     private final NotificationService notificationService;
 
+    @Async
     @TransactionalEventListener
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void on(MessageCreatedEvent event) {
         notificationService.createMessageNotification(event);
     }
 
-    // Requires_New를 서비스에 걸면 위험해 리스너 쪽에 애너테이션 추가
-    // Q) 리스너에서 서비스로 notification 생성 로직을 아예 위임 vs Dto로 변환해서 create 사용 ?
+    @Async
     @TransactionalEventListener
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void on(RoleUpdatedEvent event) {
-
         NotificationCreateRequest request = new NotificationCreateRequest(
                 event.getUserId(),
                 "권한이 변경되었습니다.",
                 event.getOldRole() + "->" + event.getNewRole()
         );
-
         notificationService.create(request);
     }
 }
