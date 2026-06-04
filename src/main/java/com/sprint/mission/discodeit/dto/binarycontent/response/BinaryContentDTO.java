2e20362104c66b30ccf76e7b9642bb7a63344dbe
jsj <sungju3115@naver.com>
2026-06-04 14:30:35 +0900

    feat: BinaryContentDTO에 status 필드 추가

--- src/main/java/com/sprint/mission/discodeit/dto/binarycontent/response/BinaryContentDTO.java
+++ src/main/java/com/sprint/mission/discodeit/dto/binarycontent/response/BinaryContentDTO.java
@@ -1,11 +1,14 @@
 package com.sprint.mission.discodeit.dto.binarycontent.response;
 
+import com.sprint.mission.discodeit.entity.BinaryContentStatus;
+
 import java.util.UUID;
 
 public record BinaryContentDTO(
         UUID id,
         String fileName,
         long size,
-        String contentType
+        String contentType,
+        BinaryContentStatus status
 ) {
 }
