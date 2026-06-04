fd291219c2810c068072c1f8f265f2c25ef852e9
jsj <sungju3115@naver.com>
2026-06-04 15:28:16 +0900

    feat: Retry 애너테이션 추가

--- src/main/java/com/sprint/mission/discodeit/config/AsyncConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/AsyncConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskDecorator;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.core.context.SecurityContext;
@@ -14,6 +15,7 @@ import java.util.Map;
 
 @Configuration
 @EnableAsync
+@EnableRetry
 public class AsyncConfig {
 
     @Bean
