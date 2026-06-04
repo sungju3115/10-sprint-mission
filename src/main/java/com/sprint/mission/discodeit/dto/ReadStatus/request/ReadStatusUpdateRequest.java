0102b6b1becd7ef30c4e15fab109225e40667dc5
jsj <sungju3115@naver.com>
2026-06-04 13:52:15 +0900

    fix: NotNull 설정 제거

--- src/main/java/com/sprint/mission/discodeit/dto/ReadStatus/request/ReadStatusUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/ReadStatus/request/ReadStatusUpdateRequest.java
@@ -7,7 +7,6 @@ import java.time.Instant;
 @Schema(description = "읽음 상태 수정 요청")
 public record ReadStatusUpdateRequest(
         @Schema(description = "새롭게 갱신할 마지막 읽은 시점", example = "2026-02-24T17:00:00Z")
-        @NotNull(message = "갱신할 시간 정보는 필수입니다.")
         Instant newLastReadAt,
         Boolean newNotificationEnabled
 ) {
