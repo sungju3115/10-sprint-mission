52315cd35215cfb3c2da3aff05d7354c567987c2
jsj <sungju3115@naver.com>
2026-06-04 09:33:17 +0900

    feat: BinaryContent status 업데이트 메서드 구현 및 추가 로직 수정

--- src/main/java/com/sprint/mission/discodeit/event/BinaryContentEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/BinaryContentEventListener.java
@@ -1,8 +1,12 @@
 package com.sprint.mission.discodeit.event;
 
+import com.sprint.mission.discodeit.entity.BinaryContentStatus;
+import com.sprint.mission.discodeit.service.BinaryContentService;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -10,10 +14,17 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @RequiredArgsConstructor
 public class BinaryContentEventListener {
     private final BinaryContentStorage binaryContentStorage;
+    private final BinaryContentService binaryContentService;
 
-    // 메타 데이터 커밋 이후 바이너리 데이터 저장 : AFTER_COMMIT
+    // 메타 데이터 커밋 이후 바이너리 데이터 저장 : AFTER_COMMIT , 트랜잭션 강제 참여
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void handleBinaryContentCreatedEvent(BinaryContentCreatedEvent event) {
-        binaryContentStorage.put(event.getBinaryContentId(), event.getBytes());
+        try{
+            binaryContentStorage.put(event.getBinaryContentId(), event.getBytes());
+            binaryContentService.updateStatus(event.getBinaryContentId(), BinaryContentStatus.SUCCESS);
+        }catch(Exception e){
+            binaryContentService.updateStatus(event.getBinaryContentId(), BinaryContentStatus.FAIL);
+        }
     }
 }
--- src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
@@ -1,9 +1,16 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.entity.BinaryContentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface BinaryContentRepository extends JpaRepository<BinaryContent, UUID> {
+
+    @Modifying
+    @Query("UPDATE BinaryContent bc SET bc.status = :status WHERE bc.id = :id")
+    void updateStatus(UUID id, BinaryContentStatus status);
 }
