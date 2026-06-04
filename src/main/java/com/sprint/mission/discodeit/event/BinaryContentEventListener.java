a0d682deef06ec4433f637b09aa121dc58c63663
jsj <sungju3115@naver.com>
2026-06-04 09:39:31 +0900

    fix: 트랜잭션 점유 변경

--- src/main/java/com/sprint/mission/discodeit/event/BinaryContentEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/BinaryContentEventListener.java
@@ -5,8 +5,6 @@ import com.sprint.mission.discodeit.service.BinaryContentService;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -16,14 +14,16 @@ public class BinaryContentEventListener {
     private final BinaryContentStorage binaryContentStorage;
     private final BinaryContentService binaryContentService;
 
-    // 메타 데이터 커밋 이후 바이너리 데이터 저장 : AFTER_COMMIT , 트랜잭션 강제 참여
+    // 메타 데이터 커밋 이후 바이너리 데이터 저장 : AFTER_COMMIT
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    // @Transactional(propagation = Propagation.REQUIRES_NEW)
+    // Todo: DB 트랜잭션 점유를 이벤트 발행 메서드로 가져가는 것 보다는 Status Update만 점유하도록 하는 게 맞나 ?
+    // 트랜잭션 A -> 커밋 -> 이벤트 발행 -> storage.put -> 새 트랜잭션 (updateStatus)
     public void handleBinaryContentCreatedEvent(BinaryContentCreatedEvent event) {
-        try{
+        try {
             binaryContentStorage.put(event.getBinaryContentId(), event.getBytes());
             binaryContentService.updateStatus(event.getBinaryContentId(), BinaryContentStatus.SUCCESS);
-        }catch(Exception e){
+        } catch (Exception e) {
             binaryContentService.updateStatus(event.getBinaryContentId(), BinaryContentStatus.FAIL);
         }
     }
