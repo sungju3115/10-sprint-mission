b8f62f41cf9487e716b1bf6e79b6b626fb48e50f
jsj <sungju3115@naver.com>
2026-06-05 09:30:24 +0900

    fix: null error 추가

--- src/main/java/com/sprint/mission/discodeit/mapper/NotificationMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/NotificationMapper.java
@@ -3,9 +3,11 @@ package com.sprint.mission.discodeit.mapper;
 import com.sprint.mission.discodeit.dto.notification.NotificationDto;
 import com.sprint.mission.discodeit.entity.Notification;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = UserMapper.class)
 public interface NotificationMapper {
 
+    @Mapping(target = "receiver.id", source = "receiver_id")
     NotificationDto toDto(Notification notification);
 }
