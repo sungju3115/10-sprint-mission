7096a233f611713c8b94f0acbdb3985d945571b6
jsj <sungju3115@naver.com>
2026-06-09 08:53:36 +0900

    refactor: Jackson 역직렬화를 위한 @JsonCreator 적용

--- src/main/java/com/sprint/mission/discodeit/event/MessageCreatedEvent.java
+++ src/main/java/com/sprint/mission/discodeit/event/MessageCreatedEvent.java
@@ -1,11 +1,11 @@
 package com.sprint.mission.discodeit.event;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
 
-@RequiredArgsConstructor
 @Getter
 public class MessageCreatedEvent {
     private final UUID messageId;
@@ -14,4 +14,21 @@ public class MessageCreatedEvent {
     private final String senderName;
     private final String channelName;
     private final String content;
+
+    @JsonCreator
+    public MessageCreatedEvent(
+            @JsonProperty("messageId") UUID messageId,
+            @JsonProperty("channelId") UUID channelId,
+            @JsonProperty("senderId") UUID senderId,
+            @JsonProperty("senderName") String senderName,
+            @JsonProperty("channelName") String channelName,
+            @JsonProperty("content") String content
+    ) {
+        this.messageId = messageId;
+        this.channelId = channelId;
+        this.senderId = senderId;
+        this.senderName = senderName;
+        this.channelName = channelName;
+        this.content = content;
+    }
 }
