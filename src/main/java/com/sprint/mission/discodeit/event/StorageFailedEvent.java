96dbe311e4723022fd66ce65bf8c65b74e4616ac
jsj <sungju3115@naver.com>
2026-06-09 08:53:36 +0900

    refactor: Jackson 역직렬화를 위한 @JsonCreator 적용

--- src/main/java/com/sprint/mission/discodeit/event/StorageFailedEvent.java
+++ src/main/java/com/sprint/mission/discodeit/event/StorageFailedEvent.java
@@ -1,11 +1,20 @@
 package com.sprint.mission.discodeit.event;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
 public class StorageFailedEvent {
     private final String title;
     private final String message;
+
+    @JsonCreator
+    public StorageFailedEvent(
+            @JsonProperty("title") String title,
+            @JsonProperty("message") String message
+    ) {
+        this.title = title;
+        this.message = message;
+    }
 }
