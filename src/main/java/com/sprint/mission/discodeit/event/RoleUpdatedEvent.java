4c431d2fea7492272911b8055d893b30a354c8dd
jsj <sungju3115@naver.com>
2026-06-09 08:53:36 +0900

    refactor: Jackson 역직렬화를 위한 @JsonCreator 적용

--- src/main/java/com/sprint/mission/discodeit/event/RoleUpdatedEvent.java
+++ src/main/java/com/sprint/mission/discodeit/event/RoleUpdatedEvent.java
@@ -1,14 +1,25 @@
 package com.sprint.mission.discodeit.event;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
 
 @Getter
-@RequiredArgsConstructor
 public class RoleUpdatedEvent {
     private final UUID userId;
     private final String oldRole;
     private final String newRole;
+
+    @JsonCreator
+    public RoleUpdatedEvent(
+            @JsonProperty("userId") UUID userId,
+            @JsonProperty("oldRole") String oldRole,
+            @JsonProperty("newRole") String newRole
+    ) {
+        this.userId = userId;
+        this.oldRole = oldRole;
+        this.newRole = newRole;
+    }
 }
