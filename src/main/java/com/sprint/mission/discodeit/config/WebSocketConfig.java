0f5e371ca08f41a28adebdb7b5cb9ecf64f90073
jsj <sungju3115@naver.com>
2026-06-14 13:55:59 +0900

    feat: 웹소켓 브로커 설정

---
+++ src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.sprint.mission.discodeit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // 서버 -> 클라이언트
+        config.enableSimpleBroker("/sub");
+        // 클라이언트 -> 서버
+        config.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+}
