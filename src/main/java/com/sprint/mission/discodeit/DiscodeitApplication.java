2c77e8f06d45d6706b34a4831f1f3c82ec6eb636
jsj <sungju3115@naver.com>
2026-06-14 15:23:05 +0900

    feat: SSE cleanUp 스케줄러 동작을 위한 @EnableScheduling 추가

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -3,7 +3,9 @@ package com.sprint.mission.discodeit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class DiscodeitApplication {
 
