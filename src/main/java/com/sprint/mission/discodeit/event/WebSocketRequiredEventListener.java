f03820ed4ad002ed978cab5e0b31fac3018356e7
jsj <sungju3115@naver.com>
2026-06-15 09:25:17 +0900

    fix: WebSocket 메시지 payload를 MessageCreatedEvent 대신 MessageDTO로 전송

--- src/main/java/com/sprint/mission/discodeit/event/WebSocketRequiredEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/WebSocketRequiredEventListener.java
@@ -1,8 +1,12 @@
 package com.sprint.mission.discodeit.event;
 
+import com.sprint.mission.discodeit.dto.message.response.MessageDTO;
+import com.sprint.mission.discodeit.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -11,10 +15,13 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class WebSocketRequiredEventListener {
 
     private final SimpMessagingTemplate template;
+    private final MessageService messageService;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void handleMessage(MessageCreatedEvent event) {
         String destination = "/sub/channels." + event.getChannelId() + ".messages";
-        template.convertAndSend(destination, event);
+        MessageDTO messageDTO = messageService.find(event.getMessageId());
+        template.convertAndSend(destination, messageDTO);
     }
 }
