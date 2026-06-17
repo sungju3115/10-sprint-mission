697ccba9b34a3ab77790d799fcc9bff376671d0d
jsj <sungju3115@naver.com>
2026-06-18 08:39:53 +0900

    feat: @EnableRetry 추가로 RedisJwtRegistry의 분산 락 재시도 로직 활성화

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -3,9 +3,11 @@ package com.sprint.mission.discodeit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableRetry
 @SpringBootApplication
 public class DiscodeitApplication {
 
